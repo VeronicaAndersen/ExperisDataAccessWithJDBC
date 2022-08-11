9e9093fcd838aa53ea42558ef43190c0cc73e2b1
Veronica Andersen <veronica.andersen@se.experis.com>
2022-08-11 09:23:19 +0200

    Feat: Created folder, added 03,04,05,06.

---
+++ Scripts/06_powers.sql
@@ -0,0 +1,4 @@
+INSERT INTO SUPERPOWER (SUPERPOWER_NAME, SUPERPOWER_DESCRIPTION) VALUES ('Web Generation', 'The power to create webs out of various kinds of substances/forces. Sub-power of Constructs Creation. Variation of Binding.');
+INSERT INTO SUPERPOWER (SUPERPOWER_NAME, SUPERPOWER_DESCRIPTION) VALUES ('Enhanced Hearing', 'The power to possess a sense of hearing beyond that of the peak members of their species. Sub-power of Enhanced Senses.');
+INSERT INTO SUPERPOWER (SUPERPOWER_NAME, SUPERPOWER_DESCRIPTION) VALUES ('Muscle Manipulation', 'The ability to manipulate the muscular structure of oneself and others. Variation of Body Manipulation and Organ Manipulation.');
+INSERT INTO SUPERPOWER (SUPERPOWER_NAME, SUPERPOWER_DESCRIPTION) VALUES ('Supernatural Reflexes', 'The power to have drastically better reflexes/reaction time than what is naturally possible. Sub-power of Supernatural Condition. Advanced version of Enhanced Reflexes.');
