7f544b5ba7dba933c8b531686c8b65684416d575
Veronica Andersen <veronica.andersen@se.experis.com>
2022-08-11 09:51:53 +0200

    Feat: Added superpowers to the superheros.

--- Scripts/06_powers.sql
+++ Scripts/06_powers.sql
@@ -2,3 +2,8 @@ INSERT INTO SUPERPOWER (SUPERPOWER_NAME, SUPERPOWER_DESCRIPTION) VALUES ('Web Ge
 INSERT INTO SUPERPOWER (SUPERPOWER_NAME, SUPERPOWER_DESCRIPTION) VALUES ('Enhanced Hearing', 'The power to possess a sense of hearing beyond that of the peak members of their species. Sub-power of Enhanced Senses.');
 INSERT INTO SUPERPOWER (SUPERPOWER_NAME, SUPERPOWER_DESCRIPTION) VALUES ('Muscle Manipulation', 'The ability to manipulate the muscular structure of oneself and others. Variation of Body Manipulation and Organ Manipulation.');
 INSERT INTO SUPERPOWER (SUPERPOWER_NAME, SUPERPOWER_DESCRIPTION) VALUES ('Supernatural Reflexes', 'The power to have drastically better reflexes/reaction time than what is naturally possible. Sub-power of Supernatural Condition. Advanced version of Enhanced Reflexes.');
+
+INSERT INTO SUPERHERO_POWER (SUPER_ID, POWER_ID) VALUES (1, 3);
+INSERT INTO SUPERHERO_POWER (SUPER_ID, POWER_ID) VALUES (2, 4);
+INSERT INTO SUPERHERO_POWER (SUPER_ID, POWER_ID) VALUES (3, 1);
+INSERT INTO SUPERHERO_POWER (SUPER_ID, POWER_ID) VALUES (3, 4);
